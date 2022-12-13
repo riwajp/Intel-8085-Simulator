49960eaf4529c2d757fce4a8ca5e3df7e3a613cf
riwajp <riwajfictious@gmail.com>
2022-12-14 00:57:22 +0545

    build

--- pages/Components/Flag.js
+++ pages/Components/Flag.js
@@ -6,7 +6,7 @@ const Flag = ({ flags }) => {
       <div className="flag-container">
         <div className="flag-container-title">Flags</div>
         <div className="flag-body">
-          {Object.keys(flags).map((f) => (
+          {Object.keys(flags ? flags : {}).map((f) => (
             <div>
               {f}: {flags[f]}
             </div>
